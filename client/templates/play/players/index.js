466bb698f6782c407e2938101d6be939c764634d
Alexander Zielenski <alex@alexzielenski.com>
2014-11-10 00:16:33 -0500

    remove old code

--- client/templates/play/players/index.js
+++ client/templates/play/players/index.js
@@ -64,32 +64,3 @@ template.events({
             Router.go("player.detail", { _id: this._id });
     }
 });
-/*
-Template.playerSearch.helpers({
-    searchResults: function () {
-        var query = Session.get("findUserQuery");
-        if (query && query.length) {
-            var expression = new RegExp(".*" + query + ".*", "i");
-
-            return Users.find({ _id: { $not: Meteor.userId() }, "friends": { $not: { $in: [ Meteor.userId() ] } }, $or: [ { "profile.name": expression }, { "emails.address": expression } ] });
-        }
-        return undefined;
-    }
-});
-
-Template.playerSearch.events({
-    "click a.addButton": function (e, template) {
-        Meteor.call("invite", this._id, true, function (error, res) {
-            if (error)
-                throwError(error.reason);
-        });
-    }
-});
-
-Template.searchModal.events({
-    "submit form": function (e, template) {
-        e.preventDefault();
-        var query = $("#findUserQuery").val();
-        Session.set("findUserQuery", query);
-    }
-});*/
